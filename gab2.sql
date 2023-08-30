e5f42f2aa99203be3a3b3c32feb073576a7c0caa
Mohammed Afrid (محمد عفريد) <140589663+muhdafrid@users.noreply.github.com>
2023-08-30 16:40:32 +0530

    Add files via upload

---
+++ gab2.sql
@@ -0,0 +1,30 @@
+create database gab2;
+show tables;
+use gab2;
+drop table user;
+
+create table user (
+id bigint primary key auto_increment,
+full_name varchar(250) not null,
+address varchar(250) not null,
+phone_number varchar(250) not null,
+mail_id varchar(250) not null,
+image longblob,
+date_updated datetime
+);
+
+
+insert into user(full_name, address, phone_number, mail_id, image, date_updated) values(
+"Shahin Shifana",
+"G.R. Nagar",
+"1234567890, 1234567890",
+"shahinshifana@gmail.com",
+"",
+now()
+);
+
+select * from user;
+
+delete from user where id = 9;
+
+desc user;
