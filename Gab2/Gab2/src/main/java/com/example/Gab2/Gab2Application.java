1ed585b30a986129fd1da1f45adb4f27303c1926
Mohammed Afrid (محمد عفريد) <140589663+muhdafrid@users.noreply.github.com>
2023-08-30 15:06:06 +0530

    Add files via upload

---
+++ Gab2/Gab2/src/main/java/com/example/Gab2/Gab2Application.java
@@ -0,0 +1,13 @@
+package com.example.Gab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gab2Application.class, args);
+	}
+
+}
